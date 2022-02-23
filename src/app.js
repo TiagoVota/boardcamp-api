13aa4c1ffff23f715e896637c76b4a836db1bdab
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-02-23 10:34:54 -0300

    update .gitignore and remove unnecessary files

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import cors from 'cors'
 import express from 'express'
 
 import healthRouter from './routers/healthRouter.js'
-import exampleRouter from './routers/exampleRouter.js'
 
 import backMiddlewareError from './middlewares/backMiddlewareError.js'
 import serverMiddlewareError from './middlewares/serverMiddlewareError.js'
@@ -14,7 +13,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/health', healthRouter)
-app.use('/example', exampleRouter)
 
 app.use(backMiddlewareError)
 app.use(serverMiddlewareError)
--- src/utils/errorsName.js
+++ src/utils/errorsName.js
@@ -1,6 +1,4 @@
 const errorsName = [
-	'AuthError',
-	'ExampleError',
 	'SchemaError',
 ]
 
