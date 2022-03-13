397a0fe82968690b94ac62e81d6c6ea960e9667c
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-03-13 10:17:18 -0300

    Preparando revisão de código

--- src/middlewares/serverMiddlewareError.js
+++
@@ -1,8 +0,0 @@
-const serverMiddlewareError = (err, req, res, next) => {
-	console.log(`Middleware de erro:\n  ${err}`)
-	
-	return res.sendStatus(500)
-}
-
-
-export default serverMiddlewareError
--- src/routers/healthRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-
-const router = new Router()
-
-router.get('', (_, res) => res.status(200).send('I\'m alive!'))
-
-
-export default router
